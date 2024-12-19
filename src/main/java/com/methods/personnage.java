40a1a25bf8589935fccb59adcee0f6e33458df17
jasmin174 <yasminesisbane@gmail.com>
2024-12-19 15:50:32 +0100

    TEST PASSED WE HAD WHAT WE EXPECTED

--- src/main/java/com/methods/personnage.java
+++ src/main/java/com/methods/personnage.java
@@ -3,9 +3,8 @@ package com.methods;
 public class personnage {
     private String orientation ="NORD";
 public String Tourner(int fois){
- if(fois>3)
- {return "OUEST";}
-    return "SUD";
+ if(fois>3) {return "NORD";}
+    return "OUEST";
 }
 }
 
