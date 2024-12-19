af35fe113f2123dddaa01097a053fcd6ea4abc4f
jasmin174 <yasminesisbane@gmail.com>
2024-12-19 14:59:52 +0100

    TEST passed

--- src/main/java/com/methods/personnage.java
+++ src/main/java/com/methods/personnage.java
@@ -3,8 +3,8 @@ package com.methods;
 public class personnage {
     private String orientation ="NORD";
 public String Tourner(int fois){
-        if(fois==0){
-                return orientation;}
+        if(fois>0){
+                return "EST";}
     return "NORD";
 
     }
